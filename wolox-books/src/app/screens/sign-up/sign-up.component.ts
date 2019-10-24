e33dac70cd3c14e2d510121d9e9497efe9875a53
Mario Andres Portela Gaviria <mario.portela@wolox.co>
2019-10-24 15:39:12 -0500

    wip

--- wolox-books/src/app/screens/sign-up/sign-up.component.ts
+++ wolox-books/src/app/screens/sign-up/sign-up.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { UserService } from '../../services/user.service';
+
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent {
 
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private service: UserService) {
     this.form = fb.group({
       firstName: [null, Validators.required],
       lastName: [null, Validators.required],
@@ -25,12 +27,12 @@ export class SignUpComponent implements OnInit {
       last_name: post.lastName,
       email: post.email,
       password: post.password,
-      password_confirmation: post.password
+      password_confirmation: post.password,
+      locale: 'en'
     };
     console.log({ user });
-  }
 
-  ngOnInit() {
+    this.service.createUser({ user }).subscribe(
+      response => console.log(`Response: ${JSON.stringify(response)}`), error => console.log(error), () => console.log('fd'));
   }
-
 }
